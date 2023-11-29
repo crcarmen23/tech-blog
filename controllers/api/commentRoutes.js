612868fd65783edbd552f678b694e2c1b1283f2c
Carmen Regina <crcarmen23@gmail.com>
2023-11-28 21:03:31 -0600

    added initial files and started refactor

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,3 @@
+//pull in models 
+//pull in router from express
+//create route that allows to add a comment 
---
+++ public/js/profile.js
@@ -0,0 +1,61 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#post-title').value.trim();
+    const description = document.querySelector('#post-description').value.trim();
+    const picture = document.querySelector('#post-picture').value.trim();
+  
+    if (title && description && picture) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, description, picture }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create post');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/post/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+  };
+  
+  document
+    .querySelector('#create-post-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.post-list')
+    .addEventListener('click', delButtonHandler);
+  
+var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'dsyymbmvk', 
+  uploadPreset: 'style-saga'}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+      console.log('Done! Here is the image info: ', result.info); 
+      document.querySelector('#post-picture').value = result.info.secure_url;
+    }
+  }
+)
+
+document.getElementById("upload_widget").addEventListener("click", function(){
+    myWidget.open();
+  }, false);
